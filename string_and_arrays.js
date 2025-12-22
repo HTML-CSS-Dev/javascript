ff7a992b6577f6493c22fb5e32a5bb989dc3a426
HTML-CSS-Dev <ranaldu456@gmail.com>
2025-12-22 15:36:55 +0500

    new

---
+++ string_and_arrays.js
@@ -0,0 +1,52 @@
+// Oson (Easy)
+
+// 1
+
+const numbers = [1, 2 ,3 , 4, 5]
+console.log(numbers.toSorted())
+
+// 2
+
+const string = 'hello';
+console.log(string.toUpperCase())
+
+
+// 3
+const num1 = [1, 2, 3]
+num1.push(4)
+console.log(num1)
+
+// 4
+const len = [7,8,9]
+console.log(len.length)
+
+// 5
+var str2 = 'Hello World';
+console.log(str2.replace('World' , 'JavaScirpt'))
+
+// 6
+const num2 = [4, 5, 6]
+console.log(String(num2.toSpliced(1,2)))
+
+// 7
+
+let len1 = 'Izbekistan';
+console.log(len1.length) 
+
+// 8
+
+const num3 = [2,3,4]
+num3.unshift(1)
+console.log(num3)
+
+// 9 
+
+let str3 = 'JAVASCRIPT'
+console.log(str3.toLowerCase())
+
+// 10
+
+const array1 = ['apple' , 'banana' ,  'cherry']
+console.log(array1.toString())
+
+// Orta daraja
